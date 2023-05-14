90e56df3224df406c4247f2ea9d0892557f534f8
otman lahreche <otman.lahreche@gmail.com>
2023-05-14 14:02:54 +0100

    update

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const {app, BrowserWindow} = require('electron') 
-const url = require('url') 
-const path = require('path')  
+const {app, BrowserWindow} = require('electron')
+const url = require('url')
+const path = require('path')
 
-let win  
+let win
 
-function createWindow() { 
-   win = new BrowserWindow({width: 800, height: 600}) 
-   win.loadURL(url.format ({ 
-      pathname: path.join(__dirname, 'index.html'), 
-      protocol: 'file:', 
-      slashes: true 
-   })) 
-}  
+function createWindow() {
+   win = new BrowserWindow({width: 800, height: 600})
+   win.loadURL(url.format ({
+      pathname: path.join(__dirname, 'index.html'),
+      protocol: 'file:',
+      slashes: true
+   }))
+}
 
-app.on('ready', createWindow) 
+app.on('ready', createWindow)
